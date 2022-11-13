de29c1c3dc93c3ee042c5d044ab4f0954120f922
Bens <playground@Benss-MacBook-Pro.local>
2022-11-13 23:27:27 +0700

    update notes

--- client/src/features/ListCard.jsx
+++ client/src/features/ListCard.jsx
@@ -3,16 +3,22 @@ import DeleteIcon from '../assets/delete_icon.svg'
 import UpdateIcon from '../assets/update_icon.svg'
 import './listCard.css'
 
-const ListCard = ({date, todo, priority}) => {
+const ListCard = ({ date, todo, priority }) => {
   return (
     <div className='listContainer'>
-        <div className='icons'>
-            < img src={ DeleteIcon } alt=""/>
-            < img src={ UpdateIcon } alt=""/>
+      <div className='iconsList'>
+        <div className='iconDel'  onClick={()=> console.log("delete")}>
+          < img src={DeleteIcon} alt="delete"/>
+          <div className='textInfo1'>Delete</div>
         </div>
-        <div className='date'>{date}</div>
-        <div className='todoList'>{todo}</div>
-        <div>{priority ? "Penting" : "Not SO"}</div>
+        <div className='iconEdit' onClick={()=> console.log("edit")}>
+          < img src={UpdateIcon} alt="edit"/>
+          <div className='textInfo2'>Edit</div>
+        </div>
+      </div>
+      <div className='date'>{date}</div>
+      <div className='todoList'>{todo}</div>
+      <div className={priority ? 'important' : null}> {priority ? "PENTING" : "Not SO"}</div>
     </div>
   )
 }
