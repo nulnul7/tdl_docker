45303feb057aee333ae443bcb60cb633dc954d42
bens <mangga.asem@gmail.com>
2023-11-27 16:04:34 +0700

    commit ketiga

--- server/server.js
+++ server/server.js
@@ -4,13 +4,6 @@ import dotenv from 'dotenv'
 import userRoute from './routes/userRoute.js'
 import todoRoute from './routes/todoRoute.js'
 import cors from 'cors'
-// import path from 'path'
-// import { fileURLToPath } from 'url';
-
-
-// const __filename = fileURLToPath(import.meta.url);
-
-// const __dirname = path.dirname(__filename);
 
 const app = express();
 dotenv.config();
